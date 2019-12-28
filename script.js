71d6d1f2ea8a9f4ea3e2679e4b2040789804ce2f
shuaib <wales149@gmail.com>
2019-12-28 02:20:17 -0800

    fixed api request issue

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-    axios.get('http://www.omdbapi.com/?apikey=51a4b74e&s='+searchText)
+    axios.get('https://www.omdbapi.com/?apikey=51a4b74e&s='+searchText)
     .then((response) => {
         console.log(response);
         let movies = response.data.Search;
